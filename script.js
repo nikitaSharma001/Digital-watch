b5a82df8001ba281ab42777db874e71493fe24d5
Nikita Sharma <nikitasharma8854@gmail.com>
2023-11-13 17:15:00 +0530

    created digital watch

---
+++ script.js
@@ -0,0 +1,15 @@
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours().toString().padStart(2, '0');
+    var minutes = now.getMinutes().toString().padStart(2, '0');
+    var seconds = now.getSeconds().toString().padStart(2, '0');
+
+    var timeString = hours + ':' + minutes + ':' + seconds;
+    document.getElementById('time').innerText = timeString;
+}
+
+// Update time every second
+setInterval(updateTime, 1000);
+
+// Initial call to display time immediately
+updateTime();
